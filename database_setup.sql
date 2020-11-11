9845762bd72cb589d3752e6181515efbba3866d8
Rui-Qi-Zhang <40679615+Rui-Qi-Zhang@users.noreply.github.com>
2020-11-10 19:54:20 -0800

    ON DELETE CASADE removed from lives

--- database_setup.sql
+++ database_setup.sql
@@ -63,13 +63,13 @@ create table Lives_Plant(
 	plantID		    integer,
     habID			integer,
     PRIMARY KEY(plantID),
-	FOREIGN KEY(habID) REFERENCES Habitat ON DELETE CASCADE);  --ON DELETE NO ACTION ON UPDATE CASCADE
+	FOREIGN KEY(habID) REFERENCES Habitat);  --ON DELETE NO ACTION ON UPDATE CASCADE
 
 create table Lives_A(
     aID			integer,
     habID			integer,
     PRIMARY KEY(aID),
-	FOREIGN KEY(habID) REFERENCES Habitat ON DELETE CASCADE);  -- ON DELETE NO ACTION ON UPDATE CASCADE
+	FOREIGN KEY(habID) REFERENCES Habitat);  -- ON DELETE NO ACTION ON UPDATE CASCADE
 
 create table Organization(
     org_name        char(40),
