2b341688d027017bb6de41a37eac8987673b12a2
Rui-Qi-Zhang <40679615+Rui-Qi-Zhang@users.noreply.github.com>
2020-11-21 09:46:49 -0800

    text format test 6

--- database.php
+++ database.php
@@ -49,7 +49,7 @@
 
 <hr />
 
-<iframe width="1" height="1" src="https://www.youtube.com/embed/ut2KhcNtnm8?autoplay=1&start=60" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="1" height="1" src="https://www.youtube.com/embed/ut2KhcNtnm8?autoplay=1&start=60&controls=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 <hr />
 <h2>Reset</h2>
